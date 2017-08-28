c0ed4bbbbfbbe9c73f25c3bf1a0deeea5186b4e5
Heather Herbert <scott.a.herbert@googlemail.com>
2017-08-28 23:21:21 +0100

    Added FAT12 header and removed print code

--- Bootloader/boot.asm
+++ Bootloader/boot.asm
@@ -9,42 +9,48 @@
 ;	Here should be the JMP followed by the FAT disk table
 ;
 
+
+        jmp     short   start                   ; MS-DOS/Windows checks for this jump
+        nop
+bsOemName               DB      "BootProg"      ; 0x03
+
+bpbBytesPerSector       DW      ?               ; 0x0B
+bpbSectorsPerCluster    DB      ?               ; 0x0D
+bpbReservedSectors      DW      ?               ; 0x0E
+bpbNumberOfFATs         DB      ?               ; 0x10
+bpbRootEntries          DW      ?               ; 0x11
+bpbTotalSectors         DW      ?               ; 0x13
+bpbMedia                DB      ?               ; 0x15
+bpbSectorsPerFAT        DW      ?               ; 0x16
+bpbSectorsPerTrack      DW      ?               ; 0x18
+bpbHeadsPerCylinder     DW      ?               ; 0x1A
+bpbHiddenSectors        DD      ?               ; 0x1C
+bpbTotalSectorsBig      DD      ?               ; 0x20
+
+bsDriveNumber           DB      ?               ; 0x24
+bsUnused                DB      ?               ; 0x25
+bsExtBootSignature      DB      ?               ; 0x26
+bsSerialNumber          DD      ?               ; 0x27
+bsVolumeLabel           DB      "SEONAIDH   "   ; 0x2B
+bsFileSystem            DB      "FAT12   "      ; 0x36
+
+
 start:
-	mov ax, 07C0h		; Set up 4K stack space after this bootloader
-	add ax, 288		; (4096 + 512) / 16 bytes per paragraph
-	mov ss, ax
-	mov sp, 4096
 
-	mov ax, 07C0h		; Set data segment to where we're loaded
-	mov ds, ax
+	cld
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
-; Once the PoC's done load the next disk sectors here
-;
-;
-
-	mov si, text_string	; Put string position into SI
-	call print_string	; Call our string-printing routine
-
-	jmp $			; Jump here - infinite loop!
+        int     12h
+        shl     ax, 6  
 
 
-	text_string db 'Seonaidh By Heather Herbert', 0
+        sub     ax, 512 / 16    
+        mov     es, ax          
+
+        sub     ax, 2048 / 16   
+        mov     ss, ax          
+        mov     sp, 2048        
 
 
-print_string:			; Routine: output string in SI to screen
-	mov ah, 0Eh		; int 10h 'print char' function
-
-.repeat:
-	lodsb			; Get character from string
-	cmp al, 0
-	je .done		; If char is zero, end of string
-	int 10h			; Otherwise, print it
-	jmp .repeat
-
-.done:
-	ret
 
 
 	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
