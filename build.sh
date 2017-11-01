9e5e5f5a32922d3097e0586ff03ae1ea036fed21
Heather Herbert <scott.a.herbert@googlemail.com>
2017-11-01 21:13:12 +0000

    Move into different folders

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ rm floppy.img
 rm SEONAIDH.com
 
 ## Assemble the bootsector + kernal
-nasm -f bin -o boot.bin boot.asm
-nasm -f bin -o SEONAIDH.com SEONAIDH.asm
+nasm -f bin -o boot.bin Bootloader/boot.asm
+nasm -f bin -o SEONAIDH.com Kernal/SEONAIDH.asm
 
 mkfs.msdos -C ./floppy.img 1440
 
@@ -20,16 +20,13 @@ dd if=boot.bin of=floppy.img bs=512 count=1 conv=notrunc
 ## Format the disk
 #mkfs.msdos -F 12 -n SEONAIDH -I ./floppy.img
 
-mkdir /media/floppy
+mkdir -p /media/floppy
 sudo mount -o loop ./floppy.img /media/floppy
 
-cp SEONAIDH.com /media/floppy/SEONAIDH.com
-#cp SEO.com /media/floppy/SEO.com
-#cp TD.EXE /media/floppy/TD.EXE
-
+sudo cp SEONAIDH.com /media/floppy/SEONAIDH.com
 ## Copy the kernal files to the disk
 
-umount /media/floppy
+sudo umount /media/floppy
 
 ## Write the boot sector
 #dd if=boot.bin of=floppy.img seek=0 count=1 conv=notrunc
